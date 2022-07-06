c81ccb1ac67009e914671fc8baf95ccdaa53c018
bBravee <jakub.kusmierczyk@op.pl>
2022-07-06 21:52:41 +0200

    First change commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Kostka 1
 var randomNumber1;
 randomNumber1 = Math.floor(Math.random()*6) + 1;
 
@@ -10,16 +9,12 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
-// Kostka 2
 var randomNumber2;
 randomNumber2 = Math.floor(Math.random()*6)+1;
 
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-// Zmiana komunikatu dla h1 określającego zwycięzcę
 
 if(randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 Wins! 🎊";
